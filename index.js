93e22296c6800afaefe53434d1e9e68a46c9873d
Konsta Nenonen <konstanenonen@protonmail.com>
2022-03-07 16:08:04 +0200

    Guest json is formated now to guestbook route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const guests = require("./guests.json");
 const express = require("express");
 const app = express();
 
@@ -6,7 +7,14 @@ app.get("/", (req, res) => {
 })
 
 app.get("/guestbook", (req, res) => {
-  res.send("Guestbook sivu");
+  const guestsFormat = guests.map(guest => (
+    `<tr><td>${guest.id}</td><td>${guest.username}</td><td>${guest.country}</td><td>${guest.date}</td><td>${guest.message}</td></tr>`
+  ))
+  .reduce((prevValue, curValue) => prevValue + curValue);
+
+  const guestTable = `<table><thead><tr><td>ID</td><td>Name</td><td>Country</td><td>Date</td><td>Message</td></tr></thead><tbody>${guestsFormat}</tbody></table>`;
+
+  res.send(guestTable);
 })
 
 app.get("/newmessage", (req, res) => {
